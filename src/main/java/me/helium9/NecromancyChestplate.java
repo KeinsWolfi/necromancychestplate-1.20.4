17849678c747c1a4a5d5b0706722ced00eeb3e76
KeinsWolfi <nils.zander2008@gmail.com>
2024-02-14 13:14:29 +0100

    Initial commit

---
+++ src/main/java/me/helium9/NecromancyChestplate.java
@@ -0,0 +1,17 @@
+package me.helium9;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NecromancyChestplate implements ModInitializer {
+	public static final String MOD_ID = "necromancychestplate";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/me/helium9/NecromancyChestplateClient.java
@@ -0,0 +1,10 @@
+package me.helium9;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NecromancyChestplateClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
